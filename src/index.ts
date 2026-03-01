814ab5e034de1046e4da72dfec203894b15cd1a0
hitesh-new <hiteshcreative001@gmail.com>
2026-03-01 23:00:29 +0530

    implemented site24/7x

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import AgentAPI from 'apminsight';
+AgentAPI.config();
+
 import express from "express";
 import subjectRouter from './routes/subjects';
 import cors from "cors";
 import securityMiddleware from './middleware/security';
 import { auth } from './lib/auth';
 import { toNodeHandler } from 'better-auth/node';
+
 const app = express();
 
 if(!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL is missing");
