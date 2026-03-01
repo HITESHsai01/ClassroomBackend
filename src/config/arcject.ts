753e99b4af496a2078b70a929419109ad309e8fe
hitesh-new <hiteshcreative001@gmail.com>
2026-03-01 19:45:21 +0530

    implemented security

---
+++ src/config/arcject.ts
@@ -0,0 +1,35 @@
+import arcjet, { slidingWindow, shield, detectBot, tokenBucket } from '@arcjet/node';
+
+if(!process.env.ARCJET_KEY && process.env.NODE_ENV !== 'test') {
+  throw new Error("ARCJET_KEY is required");
+}
+
+const aj = arcjet({
+  // Get your site key from https://app.arcjet.com and set it as an environment
+  // variable rather than hard coding.
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+   slidingWindow({
+     max:5,
+     interval:'2s',
+     mode: 'LIVE',
+   })
+  ],
+});
+
+export default aj;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import subjectRouter from './routes/subjects';
 import cors from "cors";
+import securityMiddleware from './middleware/security';
 const app = express();
 
 if(!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL is missing");
-
-app.use(express.json());
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   methods:['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
 }))
+
+app.use(express.json());
+
+app.use(securityMiddleware);
 
 app.use('/api/subjects',subjectRouter)
 
